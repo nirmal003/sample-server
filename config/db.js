afd64700865d2fb6b964619df83789aae224622f
Nirmal <nirmal.raj@recibotech.com>
2025-01-17 18:29:05 +0530

    minor changes

--- config/db.js
+++ config/db.js
@@ -5,8 +5,8 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useCreateIndex: true
       })
+      mongoose.set('strictQuery', false);
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (error) {
     console.log(error)
