85718e68e985fa296e2316e3296f0309e2d8f95b
Nirmal <nirmal.raj@recibotech.com>
2025-01-17 16:03:49 +0530

    minor changes

--- server.js
+++ server.js
@@ -21,10 +21,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../sample-client/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'sample-client', 'build', 'index.html')
-    )
-  );
+    res.sendFile(path.resolve(__dirname, '../', 'sample-client', 'build', 'index.html')));
 } else {
   app.get('/', (req, res) => res.send('Please set to production'));
 }
