2fed90d38ccd4136332ea001a8143c31f1be1f97
Nirmal <nirmal.raj@recibotech.com>
2025-01-17 19:08:17 +0530

    minor changes

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       })
-      console.log(`MongoDB Connected: ${conn.connection.host}`)
+      console.log(`MongoDB Connected: ${conn.connection.host} properly`)
   } catch (error) {
     console.log(error)
     process.exit(1)
