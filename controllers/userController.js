d94321b932e31dd79d4bf4139c20b11ff0ac23fe
Nirmal <nirmal.raj@recibotech.com>
2025-01-18 14:20:06 +0530

    minor changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,6 +56,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email })
 
   if (user && (await bcrypt.compare(password, user.password))) {
+    console.log(`user successfully logged in ${email}`);
     res.json({
       _id: user.id,
       name: user.name,
